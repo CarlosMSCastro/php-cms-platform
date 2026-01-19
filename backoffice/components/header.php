99dd497371588edb3453d78bdcfd910a5dbaa961
Carlos <carloscastro96@live.com.pt>
2026-01-19 23:31:40 +0000

    Update favicon path in header.php for consistency across components

--- backoffice/components/header.php
+++ backoffice/components/header.php
@@ -8,6 +8,7 @@
   <link href="https://fonts.googleapis.com/css2?family=Quicksand:wght@400;600;700&display=swap" rel="stylesheet">
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet">
   <link rel="stylesheet" href="css/style.css">
+  <link rel="icon" type="image/png" href="../imagens/LogotipoComunicacoes3.png">
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js"></script>
   <script src="https://cdn.tiny.cloud/1/rs29wma7uac7rjhzhyl9qmqgxo0kg75mtt9yzr22vac8erjl/tinymce/7/tinymce.min.js" referrerpolicy="origin"></script>
   <script src="js/tinymce_config.js"></script>
--- components/header.php
+++ components/header.php
@@ -24,7 +24,7 @@ if (!empty($tipoPagina) && $tipoPagina === 'home') {
 
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
-  <link rel="icon" type="image/png" href="imagens/LogotipoComunicacoes.png">
+  <link rel="icon" type="image/png" href="imagens/LogotipoComunicacoes2.png">
   <link rel="preload" href="fonts/ProximaNovaExCn-Regular.woff2" as="font" type="font/woff2" crossorigin>
   <link rel="preload" href="fonts/ProximaNovaExCn-Semibold.woff2" as="font" type="font/woff2" crossorigin>
   <link rel="stylesheet" href="css/style.css">
